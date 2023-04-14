529f677e2ee75f252983720f5e923b1801bdd1b3
Cemothy <Robin.Stockinger@gmx.de>
2023-04-14 07:51:27 +0200

    test

--- src/main/scala/Field.scala
+++ src/main/scala/Field.scala
@@ -11,7 +11,7 @@ object Field {
   val board = Array.ofDim[String](size, size)
   for (row <- 0 until size) {
     for (col <- 0 until size) {
-      board(row)(col) = "x"
+      board(row)(col) = "okay"
     }
     board(row)(size-1) = "+\n"
   }
