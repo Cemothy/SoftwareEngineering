33f383c3bde929ebdce88e6473c79848be1d0ea2
Cemothy <Robin.Stockinger@gmx.de>
2023-05-12 08:21:46 +0200

    ACM

--- src/main/scala/model/Matrix.scala
+++ src/main/scala/model/Matrix.scala
@@ -13,5 +13,31 @@ case class Matrix[T](board: Vector[Vector[T]]) {
 
     def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(board.updated(row, board(row).updated(col,cell)))
 
+    
 
+        
+    //Checked ob es für den gegebenen Stein noch einen möglichen Zug gibt(funktioniert noch nicht richtig)
+    def possibleMove(row: Int, col: Int): Boolean = {
+        return false
+        if(cell(row,col).equals(Stone.empty)) return false
+        if(cell(row,col).equals(Stone.x)){
+            if(row == size-1) return false
+            if(col == size -1) return cell(row-1,col-1).equals(Stone.empty)
+            if(col == 0) return cell(row-1,col+1).equals(Stone.empty)
+            if(cell(row-1,col+1).equals(Stone.empty) || cell(row-1,col-1).equals(Stone.empty)){ //Corner Case führt zu NullPointer!!!
+            return true
+        } else {
+            if(row == 0) return false
+            if(col == size -1) return cell(row+1,col-1).equals(Stone.empty)
+            if(col == 0) return cell(row+1,col+1).equals(Stone.empty)
+            if(cell(row+1,col+1).equals(Stone.empty) || cell(row+1,col-1).equals(Stone.empty)){
+            return true
+            }
+        }
+        }
+        return false
+    }
+    //Ckecked ob es noch einen möglichen Zug gibt 
+   
+    
 }
